af600d5189f40504b76c4743b74886f25b816baf
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2023-03-30 16:07:40 +0300

    fix: bug in our pets section for pets-page

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -1,15 +1,12 @@
 const hamburger = document.querySelector('.hamburger-container');
 const menu = document.querySelector('.navigation');
 const navLinks = document.querySelectorAll('.navigation__element__link');
-const logo = document.querySelector('.header__logo');
 const header = document.querySelector('.header');
 const body = document.querySelector('.body');
 
 hamburger.addEventListener('click', function(){
-    header.classList.toggle('not-sticky-header');
     hamburger.classList.toggle('active-hamburger');
     menu.classList.toggle('open');
-    setTimeout(() => logo.classList.toggle('active-logo'), 400);
     body.classList.toggle('hidden');
 })
 
@@ -17,8 +14,6 @@ function closeMenu(event) {
     if (event.target.classList.contains('navigation__element__link')) {
         hamburger.classList.remove('active-hamburger');
         menu.classList.remove('open');
-        logo.classList.remove('active-logo');
-        header.classList.remove('not-sticky-header');
         body.classList.remove('hidden');
     }
 }
