14513de00a53aa9ac2825379587c95a35b2ae11f
BB-pyps <80970001+BB-pyps@users.noreply.github.com>
2023-04-05 15:29:58 +0300

    feat: add script for working of burger-menu in pets-page

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -4,18 +4,23 @@ const navLinks = document.querySelectorAll('.navigation__element__link');
 const header = document.querySelector('.header');
 const body = document.querySelector('.body');
 
+
 hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('active-hamburger');
     menu.classList.toggle('open');
     body.classList.toggle('hidden');
+    body.classList.toggle('overlay');
 })
 
 function closeMenu(event) {
-    if (event.target.classList.contains('navigation__element__link')) {
+    if (event.target.classList.contains('navigation__element__link') || event.target.classList.contains('overlay')) {
         hamburger.classList.remove('active-hamburger');
         menu.classList.remove('open');
         body.classList.remove('hidden');
+        body.classList.remove('overlay');
+
     }
 }
 
 navLinks.forEach((el) => el.addEventListener('click', closeMenu));
+body.addEventListener('click', closeMenu);
